4ab16d41a8a5956fc1816b5e99ca959cc8167250
qidong <527213991@qq.com>
2018-01-25 15:54:35 +0800

    添加第二行代码

--- app/src/main/java/com/android/ebeijia/testgitdemo2/MainActivity.java
+++ app/src/main/java/com/android/ebeijia/testgitdemo2/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String s1="dsdsd";
+
+        String s2="第二行代码";
     }
 }
